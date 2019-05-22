c72ef0a68ea5500b82bd36456735021e607eb6ec
yushimatenjin <ryuto.haga@gmail.com>
2019-05-22 17:04:38 +0900

    add endpoints

--- src/playcanvas.ts
+++ src/playcanvas.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { PlayCanvasOptions } from "./interfaces";
-
+import {Assets} from './endpoints'
 export default class PlayCanvas {
   accessToken: string;
   scenes?: Array<number>;
@@ -8,32 +8,40 @@ export default class PlayCanvas {
   branchId?: string;
   projectName?: string;
   resourceUrl?: string;
+  assetId?: string;
+  headers: {
+    "Authorization": string;
+    "Content-Type": string
+  }
 
   constructor({
     accessToken,
     scenes,
     projectId,
     branchId,
-    projectName
+    projectName,
   }: PlayCanvasOptions) {
     this.accessToken = accessToken;
     this.scenes = scenes;
     this.projectId = projectId;
     this.branchId = branchId;
     this.projectName = projectName;
-    this.resourceUrl = `https://playcanvas.com/api/projects/${
-      this.projectId
-    }/assets?branchId=${this.branchId}`;
+    this.headers =       {
+        "Authorization": `Bearer ${this.accessToken}`,
+        "Content-Type": "application/json"
+      }
   }
 
   async listAssets() {
-    const response = await axios.get(this.resourceUrl, {
-      headers: {
-        Authorization: `Bearer ${this.accessToken}`,
-        "Content-Type": "application/json"
-      }
+    const response = await axios.get(Assets.LIST_ASSETS({projectId:this.projectId,branchId:this.branchId}), {
+       headers:this.headers 
     });
-
     return response.data;
+  }
+  async getAssets(assetId){
+    const response = await axios.get(Assets.GET_ASSETS({assetId:assetId,branchId:this.branchId}), {
+        headers:this.headers 
+     });
+     return response.data;
   }
 }
