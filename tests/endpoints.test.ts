6c0df8f8edb13a69a15218ac921e4e42031c3b92
yushimatenjin <39250588+yushimatenjin@users.noreply.github.com>
2020-03-25 09:17:15 +0900

    Fixed test

--- tests/endpoints.test.ts
+++ tests/endpoints.test.ts
@@ -5,6 +5,6 @@ const jobId = 12345
 describe('Jobs', (): void => {
     test("GET_JOBS", (): void => {
         const response: string = Jobs.GET_JOBS(jobId)
-        expect(response).toBe('https://playcanvas.com/api/api/jobs/12345');
+        expect(response).toBe('https://playcanvas.com/api/jobs/12345');
     })
 })
